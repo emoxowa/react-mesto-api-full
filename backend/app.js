310897384c4971c246d46abcc171d799e20d906a
Evgeniia <Kochetkovaev11@gmail.com>
2022-11-16 12:12:01 +0300

    fix: CORS

--- backend/app.js
+++ backend/app.js
@@ -22,12 +22,11 @@ mongoose
   });
 
 const app = express();
-
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
-app.use(cors);
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,19 @@
 const allowedCors = [
   'http://mesto.yandex.nomoredomains.icu:3001',
   'localhost:3000',
+  'http://localhost:3000',
+  'https://localhost:3000',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
 const cors = (req, res, next) => {
-  const { method } = req;
+  console.log(req.headers);
   const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
