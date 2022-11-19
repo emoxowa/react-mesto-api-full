9119a2f1bc5a38b565859c6d0a6e752f9c367c7b
Evgeniia <Kochetkovaev11@gmail.com>
2022-11-19 09:27:04 +0300

    test

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -72,7 +72,7 @@ function App() {
   
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
     console.log(card.likes);
     console.log(currentUser._id);
     console.log(isLiked);
